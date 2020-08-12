c6eb5f750bb56d827ec125afe47e4ab2408962c9
Pedro <pedrobslisboa@gmail.com>
2020-08-12 10:04:46 -0300

    feat(nextjs): add styled-components pattern, and img import sample

--- src/templates/next/src/pages/index.tsx
+++ src/templates/next/src/pages/index.tsx
@@ -1,7 +1,27 @@
 import React from 'react'
+import styled from 'styled-components'
+
+import reactImage from './react-logo.svg'
 
 const IndexPage = () => {
-  return <div>Hello world</div>
+  return (
+    <StyledWrapper>
+      <h1>Hello world!</h1>
+      <StyledImage src={reactImage} alt="React Significa" />
+    </StyledWrapper>
+  )
 }
 
 export default IndexPage
+
+const StyledWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`
+
+const StyledImage = styled.img`
+  height: 300px;
+  width: 300px;
+`
