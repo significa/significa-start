c7aca6052a1d5e323aeedcb8b76118913f8af806
Pedro <pedrobslisboa@gmail.com>
2020-08-12 10:05:49 -0300

    feat(cra): add styled-components pattern, and img import sample

--- src/templates/cra/src/App.tsx
+++ src/templates/cra/src/App.tsx
@@ -1,7 +1,27 @@
 import React from 'react'
+import styled from 'styled-components'
+
+import reactImage from './react-logo.svg'
 
 const App = () => {
-  return <div>Hello from CRA</div>
+  return (
+    <StyledWrapper>
+      <h1>Hello world!</h1>
+      <StyledImage src={reactImage} alt="React Significa" />
+    </StyledWrapper>
+  )
 }
 
 export default App
+
+const StyledWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`
+
+const StyledImage = styled.img`
+  height: 300px;
+  width: 300px;
+`
