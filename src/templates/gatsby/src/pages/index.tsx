fbf781becf02f1ee3ebb4a111a45c6edfef3b458
Pedro <pedrobslisboa@gmail.com>
2020-08-12 10:06:07 -0300

    feat(gatsby): add styled-components pattern

--- src/templates/gatsby/src/pages/index.tsx
+++ src/templates/gatsby/src/pages/index.tsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const IndexPage = () => {
-  return <div>Hello world</div>
+  return (
+    <StyledWrapper>
+      <h1>Hello world!</h1>
+    </StyledWrapper>
+  )
 }
 
 export default IndexPage
+
+const StyledWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`
